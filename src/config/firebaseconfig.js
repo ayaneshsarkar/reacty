fd2bcfa8be850d31c6a6074f8ca96c9add8a1e85
Ayanesh Frankenstein <39999169+ayaneshsarkar@users.noreply.github.com>
2021-02-05 23:46:06 +0530

    Create firebaseconfig.js

---
+++ src/config/firebaseconfig.js
@@ -0,0 +1,14 @@
+import 'firebase/firestore';
+
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyAE-R72KjQ31AI5NLygnBqDLd6tqTAhjgo",
+  authDomain: "reacty-1b779.firebaseapp.com",
+  databaseURL: "https://reacty-1b779.firebaseio.com",
+  projectId: "reacty-1b779",
+  storageBucket: "reacty-1b779.appspot.com",
+  messagingSenderId: "232505252347",
+  appId: "1:232505252347:web:1bead0575f3b459be6535e",
+  measurementId: "G-8SETD7KDVV"
+};
+export default firebaseConfig;
